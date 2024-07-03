384fdfef2ac2047c402768c4bc1c524cafa070e3
KTV02 <l.kremp@gmx.de>
2024-07-03 20:48:37 +0200

    Updated Crawler to use hostname for remote db

--- webcrawler/test.py
+++ webcrawler/test.py
@@ -1,9 +1,23 @@
 import requests
 
 # Use the internal IP address of your Raspberry Pi
-url = 'http://100.113.220.63:5000/query'
-query = 'SELECT * FROM documents LIMIT 10'
+url = 'http://l.kremp-everest.nord:5000/query'
+query = 'SELECT COUNT(*) AS count FROM documents'
 
-response = requests.post(url, json={'query': query})
-data = response.json()
-print(data)
+# Basic authentication details
+auth = ('mseproject', 'tuebingen2024')
+
+# Make the POST request to the Flask API with the query
+response = requests.post(url, json={'query': query}, auth=auth)
+
+# Check if the request was successful
+if response.status_code == 200:
+    # Parse the JSON response
+    data = response.json()
+    if data:
+        count = data[0]['count']
+        print(f"Total documents count: {count}")
+    else:
+        print("No data returned.")
+else:
+    print(f"Error executing query: {response.status_code} - {response.text}")
